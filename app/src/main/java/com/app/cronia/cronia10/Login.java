c90455c52f55dfae9e2cef643e452395a40b6ca3
BILGEADAM\deniz.alkan <denizalkan95.da@gmail.com>
2018-08-04 14:25:34 +0300

    Yorum satırı eklendi

--- app/src/main/java/com/app/cronia/cronia10/Login.java
+++ app/src/main/java/com/app/cronia/cronia10/Login.java
@@ -1,13 +1,13 @@
 package com.app.cronia.cronia10;
 
-import android.content.Intent;
-import android.net.Uri;
-import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
-import android.view.Display;
-import android.view.View;
-import android.widget.Button;
+        import android.content.Intent;
+        import android.net.Uri;
+        import android.os.Bundle;
+        import android.support.annotation.Nullable;
+        import android.support.v7.app.AppCompatActivity;
+        import android.view.Display;
+        import android.view.View;
+        import android.widget.Button;
 
 public class Login extends AppCompatActivity {
     //Butonumuz için değişken oluşturuyoruz.
@@ -32,3 +32,5 @@ public class Login extends AppCompatActivity {
          //deneme
     }
 }
+
+// ömer can burayı görmen lazım
