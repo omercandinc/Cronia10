c271374cdb0b91b12d0892d65067dcbc1e6cbb48
fatihgokcee <hmzkts@gmail.com>
2018-09-01 09:33:27 +0300

    Register java ile register layout birleştirildi.

--- app/src/main/java/com/app/cronia/cronia10/Register.java
+++ app/src/main/java/com/app/cronia/cronia10/Register.java
@@ -1,4 +1,14 @@
 package com.app.cronia.cronia10;
 
-public class Register {
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Register extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register);
+    }
+
 }
