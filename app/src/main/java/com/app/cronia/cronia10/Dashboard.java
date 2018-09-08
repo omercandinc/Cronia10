771d53920c3030d445ff319c646a50ec7951901c
BILGEADAM\deniz.alkan <denizalkan95.da@gmail.com>
2018-09-08 03:50:49 +0300

    Dashboard Oluştu

--- app/src/main/java/com/app/cronia/cronia10/Dashboard.java
+++ app/src/main/java/com/app/cronia/cronia10/Dashboard.java
@@ -1,4 +1,14 @@
 package com.app.cronia.cronia10;
 
-public class Dashboard {
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class Dashboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dashboard);
+    }
 }
