09f1560db7e74d7b0cb673f15342b68ba04aebf3
Ömercan Dinç <omercandinc4152@hotmail.com>
2018-09-08 10:06:26 +0300

    footer button intent main timerlar eklendi

--- app/src/main/java/com/app/cronia/cronia10/MainActivity.java
+++ app/src/main/java/com/app/cronia/cronia10/MainActivity.java
@@ -1,12 +1,16 @@
 package com.app.cronia.cronia10;
 
+import android.content.Intent;
 import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Chronometer;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     private CardView card_food,card_read;
@@ -14,6 +18,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private Chronometer mChronometer_food;
     private int durum= 0;
     private String deneme;
+    private ImageButton Footer_ImgBtn_Dashboard;
 
 
 
@@ -24,20 +29,32 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 
 
+
+        // chronometers
         mChronometer_food = (Chronometer) findViewById(R.id.mChronometer_food);
 
         //cardview nesnelerinin tanımları
         card_food= (CardView) findViewById(R.id.card_food);
         card_read = (CardView) findViewById(R.id.card_read);
+        Footer_ImgBtn_Dashboard = (ImageButton) findViewById(R.id.Footer_ImgBtn_Dashboard);
+
 
         //click listener to the cards
         card_food.setOnClickListener(this);
         card_read.setOnClickListener(this);
+        Footer_ImgBtn_Dashboard .setOnClickListener(this);
+
+        //chronometers invisible
+        mChronometer_food.setVisibility(View.INVISIBLE);
 
 
 
 
     }
+
+
+
+
 
     @Override
     public void onClick(View view) {
@@ -65,6 +82,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     durum = 0;
 
                 }
+
+                break;
+
+
+            case R.id.Footer_ImgBtn_Dashboard :
+
+                Intent i=new Intent();
+                i.setClass(MainActivity.this,Dashboard.class);
+                startActivity(i);
 
 
         }
