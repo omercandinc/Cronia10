3e37090634323f581ba95d7e84fde1606f5f6bc7
Ömercan Dinç <omercandinc4152@hotmail.com>
2018-09-08 03:08:41 +0300

    timer tamamlandı

--- app/src/main/java/com/app/cronia/cronia10/MainActivity.java
+++ app/src/main/java/com/app/cronia/cronia10/MainActivity.java
@@ -1,6 +1,6 @@
 package com.app.cronia.cronia10;
 
-import android.os.CountDownTimer;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
@@ -8,19 +8,13 @@ import android.view.View;
 import android.widget.Chronometer;
 import android.widget.TextView;
 
-import java.util.Locale;
-
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     private CardView card_food,card_read;
-    private Chronometer mChronometer;
-    private long lastPause;
+    private int Cardview_ID;
+    private Chronometer mChronometer_food;
+    private int durum= 0;
+    private String deneme;
 
-    private static final long START_TIME_IN_MILLIS = 600000;
-
-    private TextView txtTimer_food;
-    private CountDownTimer mCountDownTimer;
-    private boolean mTimerRunning;
-    private long mTimeLeftInMillis = START_TIME_IN_MILLIS;
 
 
     /*yorum*/
@@ -29,8 +23,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        txtTimer_food = (TextView) findViewById(R.id.txtTimer_food);
 
+        mChronometer_food = (Chronometer) findViewById(R.id.mChronometer_food);
 
         //cardview nesnelerinin tanımları
         card_food= (CardView) findViewById(R.id.card_food);
@@ -40,6 +34,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         card_food.setOnClickListener(this);
         card_read.setOnClickListener(this);
 
+
+
+
     }
 
     @Override
@@ -48,56 +45,35 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         switch (view.getId())
         {
-            case R.id.card_food :  if (mTimerRunning) {
-                pauseTimer();
-            } else {
-                startTimer();
-            }
+
+
+            case R.id.card_food :
+
+                if ( durum == 0)
+                {
+                    mChronometer_food.setBase(SystemClock.elapsedRealtime());
+                    mChronometer_food.start();
+                    mChronometer_food.setVisibility(View.VISIBLE);
+                    durum = 1;
+                }
+
+                else
+                {
+                    mChronometer_food.setBase(SystemClock.elapsedRealtime());
+                    mChronometer_food.stop();
+                    mChronometer_food.setVisibility(View.INVISIBLE);
+                    durum = 0;
+
+                }
+
+
         }
 
 
 
     }
 
-    private void startTimer() {
-        mCountDownTimer = new CountDownTimer(mTimeLeftInMillis, 1000) {
-            @Override
-            public void onTick(long millisUntilFinished) {
-                mTimeLeftInMillis = millisUntilFinished;
-                updateCountDownText();
-            }
 
-            @Override
-            public void onFinish() {
-                mTimerRunning = false;
-
-            }
-        }.start();
-
-        mTimerRunning = true;
-
-    }
-
-    private void pauseTimer() {
-        mCountDownTimer.cancel();
-        mTimerRunning = false;
-
-    }
-
-    private void resetTimer() {
-        mTimeLeftInMillis = START_TIME_IN_MILLIS;
-        updateCountDownText();
-
-    }
-
-    private void updateCountDownText() {
-        int minutes = (int) (mTimeLeftInMillis / 1000) / 60;
-        int seconds = (int) (mTimeLeftInMillis / 1000) % 60;
-
-        String timeLeftFormatted = String.format(Locale.getDefault(), "%02d:%02d", minutes, seconds);
-
-        txtTimer_food.setText(timeLeftFormatted);
-    }
 
 
 
