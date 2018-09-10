db6d93d0560e36a8a0004d851dbe8cb689358dd2
Ömercan Dinç <omercandinc4152@hotmail.com>
2018-09-10 18:26:00 +0300

    Kronometreler ayarlandı. başka kronometreye tıklayınca diğer kronometreler pasif hale getirildi main bottom bar links tammamlandı (çift yönlü)

--- app/src/main/java/com/app/cronia/cronia10/MainActivity.java
+++ app/src/main/java/com/app/cronia/cronia10/MainActivity.java
@@ -13,11 +13,9 @@ import android.widget.TextView;
 import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
-    private CardView main_cardvw_1_1,main_cardvw_1_2;
-    private int Cardview_ID;
-    private Chronometer mChronometer_food;
-    private int durum= 0;
-    private String deneme;
+    public CardView main_cardvw_1_1,main_cardvw_1_2, main_cardvw_2_1, main_cardvw_2_2, main_cardvw_3_1, main_cardvw_3_2;
+    public Chronometer main_chr_1_1,main_chr_1_2,main_chr_2_1,main_chr_2_2,main_chr_3_1,main_chr_3_2;
+    public int durum_1_1= 0 , durum_1_2 = 0, durum_2_1 = 0, durum_2_2 = 0 ,durum_3_1 = 0 , durum_3_2 = 0;
     private ImageButton footer_imgbtn_dashboard;
     private ImageButton footer_imgbtn_home;
     private ImageButton footer_imgbtn_profile;
@@ -33,33 +31,89 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
 
         // chronometers
-        mChronometer_food = (Chronometer) findViewById(R.id.mChronometer_food);
+        main_chr_1_1 = (Chronometer) findViewById(R.id.main_chr_1_1);
+        main_chr_1_2 = (Chronometer) findViewById(R.id.main_chr_1_2);
+        main_chr_2_1 = (Chronometer) findViewById(R.id.main_chr_2_1);
+        main_chr_2_2 = (Chronometer) findViewById(R.id.main_chr_2_2);
+        main_chr_3_1 = (Chronometer) findViewById(R.id.main_chr_3_1);
+        main_chr_3_2 = (Chronometer) findViewById(R.id.main_chr_3_2);
 
         //cardview nesnelerinin tanımları
         main_cardvw_1_1= (CardView) findViewById(R.id.main_cardvw_1_1);
         main_cardvw_1_2 = (CardView) findViewById(R.id.main_cardvw_1_2);
+        main_cardvw_2_1 = (CardView) findViewById(R.id.main_cardvw_2_1);
+        main_cardvw_2_2 = (CardView) findViewById(R.id.main_cardvw_2_2);
+        main_cardvw_3_1 = (CardView) findViewById(R.id.main_cardvw_3_1);
+        main_cardvw_3_2 = (CardView) findViewById(R.id.main_cardvw_3_2);
+
         footer_imgbtn_dashboard = (ImageButton) findViewById(R.id.footer_imgbtn_dashboard);
         footer_imgbtn_home = (ImageButton) findViewById(R.id.footer_imgbtn_home);
         footer_imgbtn_profile = (ImageButton) findViewById(R.id.footer_imgbtn_profile);
+
+        // toolbar icons on - off state
+        footer_imgbtn_dashboard.setImageResource(R.drawable.main_dashboardbutton_off);
+        footer_imgbtn_home.setImageResource(R.drawable.main_homebutton_on);
+        footer_imgbtn_profile.setImageResource(R.drawable.main_profilebutton_off);
+
+
+
 
 
         //click listener to the cards
         main_cardvw_1_1.setOnClickListener(this);
         main_cardvw_1_2.setOnClickListener(this);
+        main_cardvw_2_1.setOnClickListener(this);
+        main_cardvw_2_2.setOnClickListener(this);
+        main_cardvw_3_1.setOnClickListener(this);
+        main_cardvw_3_2.setOnClickListener(this);
+
+
         footer_imgbtn_dashboard .setOnClickListener(this);
         footer_imgbtn_home .setOnClickListener(this);
         footer_imgbtn_profile .setOnClickListener(this);
 
         //chronometers invisible
-        mChronometer_food.setVisibility(View.INVISIBLE);
-
-
-
+        main_chr_1_1.setVisibility(View.INVISIBLE);
+        main_chr_1_2.setVisibility(View.INVISIBLE);
+        main_chr_2_1.setVisibility(View.INVISIBLE);
+        main_chr_2_2.setVisibility(View.INVISIBLE);
+        main_chr_3_1.setVisibility(View.INVISIBLE);
+        main_chr_3_2.setVisibility(View.INVISIBLE);
 
     }
 
-
-
+    public void resetChr()
+    {
+        main_chr_1_1.setBase(SystemClock.elapsedRealtime());
+        main_chr_1_1.stop();
+        main_chr_1_1.setVisibility(View.INVISIBLE);
+        //
+        main_chr_1_2.setBase(SystemClock.elapsedRealtime());
+        main_chr_1_2.stop();
+        main_chr_1_2.setVisibility(View.INVISIBLE);
+        //
+        main_chr_2_1.setBase(SystemClock.elapsedRealtime());
+        main_chr_2_1.stop();
+        main_chr_2_1.setVisibility(View.INVISIBLE);
+        //
+        main_chr_2_2.setBase(SystemClock.elapsedRealtime());
+        main_chr_2_2.stop();
+        main_chr_2_2.setVisibility(View.INVISIBLE);
+        //
+        main_chr_3_1.setBase(SystemClock.elapsedRealtime());
+        main_chr_3_1.stop();
+        main_chr_3_1.setVisibility(View.INVISIBLE);
+        //
+        main_chr_3_2.setBase(SystemClock.elapsedRealtime());
+        main_chr_3_2.stop();
+        main_chr_3_2.setVisibility(View.INVISIBLE);
+        //durumlar
+        durum_1_1 = 0;
+        durum_1_2 = 0;
+        durum_2_2 = 0;
+        durum_3_1 = 0;
+        durum_3_2 = 0;
+    }
 
 
     @Override
@@ -71,45 +125,143 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
 
             case R.id.main_cardvw_1_1 :
+                    if( durum_1_1 == 0) {
+                        resetChr();
+                        main_chr_1_1.setBase(SystemClock.elapsedRealtime());
+                        main_chr_1_1.start();
+                        main_chr_1_1.setVisibility(View.VISIBLE);
+                        durum_1_1 = 1;
+                    }
 
-                if ( durum == 0)
+                    else {
+                        main_chr_1_1.setBase(SystemClock.elapsedRealtime());
+                        main_chr_1_1.stop();
+                        main_chr_1_1.setVisibility(View.INVISIBLE);
+                    }
+
+
+
+                break;
+
+            case R.id.main_cardvw_1_2 :
+                    if ( durum_1_2 == 0)
+                    {
+                        resetChr();
+                        main_chr_1_2.setBase(SystemClock.elapsedRealtime());
+                        main_chr_1_2.start();
+                        main_chr_1_2.setVisibility(View.VISIBLE);
+                        durum_1_2 = 1;
+                    }
+                    else {
+                        main_chr_1_2.setBase(SystemClock.elapsedRealtime());
+                        main_chr_1_2.stop();
+                        main_chr_1_2.setVisibility(View.INVISIBLE);
+                    }
+
+
+                break;
+
+            case R.id.main_cardvw_2_1 :
+                if ( durum_2_1 == 0)
                 {
-                    mChronometer_food.setBase(SystemClock.elapsedRealtime());
-                    mChronometer_food.start();
-                    mChronometer_food.setVisibility(View.VISIBLE);
-                    durum = 1;
+                    resetChr();
+                    main_chr_2_1.setBase(SystemClock.elapsedRealtime());
+                    main_chr_2_1.start();
+                    main_chr_2_1.setVisibility(View.VISIBLE);
+                    durum_2_1 = 1;
                 }
 
                 else
                 {
-                    mChronometer_food.setBase(SystemClock.elapsedRealtime());
-                    mChronometer_food.stop();
-                    mChronometer_food.setVisibility(View.INVISIBLE);
-                    durum = 0;
-
+                    main_chr_2_1.setBase(SystemClock.elapsedRealtime());
+                    main_chr_2_1.stop();
+                    main_chr_2_1.setVisibility(View.INVISIBLE);
                 }
+
+
+                break;
+
+            case R.id.main_cardvw_2_2 :
+                if ( durum_2_2 == 0)
+                {
+                    resetChr();
+                    main_chr_2_2.setBase(SystemClock.elapsedRealtime());
+                    main_chr_2_2.start();
+                    main_chr_2_2.setVisibility(View.VISIBLE);
+                    durum_2_2=1;
+                }
+
+                else
+                {
+                    main_chr_2_2.setBase(SystemClock.elapsedRealtime());
+                    main_chr_2_2.stop();
+                    main_chr_2_2.setVisibility(View.INVISIBLE);
+                }
+
+
+                break;
+
+            case R.id.main_cardvw_3_1 :
+                if (durum_3_1 == 0)
+                {
+                    resetChr();
+                    main_chr_3_1.setBase(SystemClock.elapsedRealtime());
+                    main_chr_3_1.start();
+                    main_chr_3_1.setVisibility(View.VISIBLE);
+                    durum_3_1=1;
+                }
+
+                else
+                {
+                    main_chr_3_1.setBase(SystemClock.elapsedRealtime());
+                    main_chr_3_1.stop();
+                    main_chr_3_1.setVisibility(View.INVISIBLE);
+                }
+
+
+                break;
+
+            case R.id.main_cardvw_3_2 :
+                if (durum_3_2==0)
+                {
+                    resetChr();
+                    main_chr_3_2.setBase(SystemClock.elapsedRealtime());
+                    main_chr_3_2.start();
+                    main_chr_3_2.setVisibility(View.VISIBLE);
+                    durum_3_2=1;
+                }
+
+                else
+                {
+                    main_chr_3_2.setBase(SystemClock.elapsedRealtime());
+                    main_chr_3_2.stop();
+                    main_chr_3_2.setVisibility(View.INVISIBLE);
+                }
+
 
                 break;
 
 
+            //bottom bar case
             case R.id.footer_imgbtn_dashboard :
 
-                Intent i=new Intent();
-                i.setClass(MainActivity.this,Dashboard.class);
-                startActivity(i);
-                footer_imgbtn_dashboard.setImageResource(R.drawable.main_dashboardbutton_on);
-                footer_imgbtn_home.setImageResource(R.drawable.main_homebutton_off);
+                Intent dashboard=new Intent();
+                dashboard.setClass(MainActivity.this,Dashboard.class);
+                startActivity(dashboard);
 
+                break;
 
+            case R.id.footer_imgbtn_profile :
+
+                Intent profile=new Intent();
+                profile.setClass(MainActivity.this,Profile.class);
+                startActivity(profile);
+
+                break;
 
         }
 
-
-
     }
-
-
-
 
 
 }
